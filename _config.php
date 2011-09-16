990ff0c1d21ec36e22de9e9259576093996e346f
Nathaniel McHugh <nat@ibuildings.com>
2011-09-16 13:08:16 +0100

    add doc blocks for the PHP license

--- _config.php
+++ _config.php
@@ -1,4 +1,18 @@
 <?php
 
+/**
+ *
+ * PHP version 5
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @package   LoggingMailer
+ * @author     Nathaniel McHugh <nat@fishtrap.co.uk>
+ */
+
 Email::set_mailer( new LoggingMailer() );
 SS_Report::register('ReportAdmin', 'SentMailReport', -10);
--- code/LoggingMailer.php
+++ code/LoggingMailer.php
@@ -1,5 +1,19 @@
 <?php
 
+/**
+ *
+ * PHP version 5
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @package   LoggingMailer
+ * @author     Nathaniel McHugh <nat@fishtrap.co.uk>
+ */
+
 class LoggingMailer extends Mailer{
    
    function sendPlain($to, $from, $subject, $plainContent, $attachedFiles = false, $customheaders = false) {
